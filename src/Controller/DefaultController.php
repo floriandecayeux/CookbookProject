2a63d8acc15aa840b2c944b2f41d7e3784a83f9e
clemv80 <clem.v80@gmail.com>
2018-05-10 18:14:28 +0200

    Page d'accueil

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: clementvacandard
+ * Date: 10/05/2018
+ * Time: 17:38
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(){
+        return $this->render('index.html.twig'/*, [
+            'reservations' => $reservations,
+            'metresReserves' => $metresReserves,
+            'sommeCollectee' => $sommeCollectee
+        ]*/);
+    }
+}
