63893264ff4b94abef4963532585ee8ace88ecd8
floriandecayeux <florian80.decayeux@gmail.com>
2018-05-25 11:12:59 +0200

    comit var user retourné defaultcontroller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -19,6 +20,21 @@ class DefaultController extends Controller
      * @Route("/", name="index")
      */
     public function index(){
-        return $this->render('index.html.twig');
+
+        $user = $this->getUser();
+
+        return $this->render('index.html.twig', array(
+            'user' => $user)/*, [
+            'reservations' => $reservations,
+            'metresReserves' => $metresReserves,
+            'sommeCollectee' => $sommeCollectee
+
+        ]*/);
     }
+
+
+
+
+
+
 }
