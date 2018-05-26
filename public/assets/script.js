08f64933c6c9051b96de719b40b964e81a80809e
clemv80 <clem.v80@gmail.com>
2018-05-26 14:45:17 +0200

    connexion

---
+++ public/assets/script.js
@@ -0,0 +1,8 @@
+let connectionModal = document.getElementById('connectionModal');
+
+function openConnectionModal(){
+    connectionModal.className = 'modal is-active';
+}
+function closeConnectionModal(){
+    connectionModal.className = 'modal';
+}
--- src/Migrations/Version20180525070730.php
+++ src/Migrations/Version20180525070730.php
@@ -8,17 +8,16 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180522075637 extends AbstractMigration
+final class Version20180525070730 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(64) NOT NULL, email VARCHAR(60) NOT NULL, is_active TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE recette (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, titre VARCHAR(25) NOT NULL, temps_preparation INT NOT NULL, nb_personnes INT NOT NULL, UNIQUE INDEX UNIQ_49BB6390FF7747B4 (titre), INDEX IDX_49BB6390A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE recette ADD CONSTRAINT FK_49BB6390A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema) : void
@@ -26,8 +25,8 @@ final class Version20180522075637 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE recette DROP FOREIGN KEY FK_49BB6390A76ED395');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE recette');
-        $this->addSql('DROP INDEX UNIQ_8D93D649F85E0677 ON user');
-        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
     }
 }
