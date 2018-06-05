245e0d0621b570e3a13598761aec9c2a760f860a
floriandecayeux <florian80.decayeux@gmail.com>
2018-06-05 09:32:05 +0200

    chcommit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,6 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Recette;
+use App\Form\RecetteType;
 use App\Entity\User;
 
 class DefaultController extends Controller
@@ -21,10 +23,18 @@ class DefaultController extends Controller
      */
     public function index(){
 
-        $user = $this->getUser();
+        $recettes = $this->getDoctrine()
+            ->getRepository(Recette::class)
+            ->findAll();
 
-        return $this->render('index.html.twig', array(
-            'user' => $user)/*, [
+        if (!$recettes) {
+            throw $this->createNotFoundException(
+                'No product found '
+            );
+        }
+
+       return $this->render('index.html.twig', array(
+            'recettes' => $recettes)/*, [
             'reservations' => $reservations,
             'metresReserves' => $metresReserves,
             'sommeCollectee' => $sommeCollectee
